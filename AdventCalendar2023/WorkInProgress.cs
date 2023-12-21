bb5f1da144610d9547a5bacae78a3f1416119bf2
Johan Björk <johanbjork@live.se>
2023-12-21 23:39:12 +0100

    Day 21 work in progress

--- AdventCalendar2023/WorkInProgress.cs
+++ AdventCalendar2023/WorkInProgress.cs
@@ -149,6 +149,8 @@ namespace AdventCalendar2023
             int maxY = y;
             int maxX = x;
             Tuple<int, int, int> fullData = Day21StepsToFull(rocks, startX, startY, maxX, maxY);
+            int remainingSteps = 26501365 % 12;
+
 
             HashSet<Day21Point> currentPositions = new HashSet<Day21Point>();
             currentPositions.Add(new Day21Point { X = startX, Y = startY });
@@ -157,6 +159,7 @@ namespace AdventCalendar2023
             List<int> diffList = new List<int>();
             List<int> diffDiffList = new List<int>();
             List<int> diffDiffDiffList = new List<int>();
+            List<int> edgePlots = new List<int>();
             for (int i = 0; i < 300; i++)
             {
                 foreach (Day21Point position in currentPositions)
@@ -179,22 +182,68 @@ namespace AdventCalendar2023
                         newPositions.Add(keyEast);
                 }
                 currentPositions = newPositions;
-                gardenPlotList.Add(currentPositions.Count());
-                if (gardenPlotList.Count() > 11)
-                    diffList.Add(gardenPlotList.Last() - gardenPlotList.SkipLast(1).First());
-                if (diffList.Count() > 11)
-                    diffDiffList.Add(diffList.Last() - diffList.SkipLast(11).First());
+                //gardenPlotList.Add(currentPositions.Count());
+                //if (gardenPlotList.Count() > 11)
+                //    diffList.Add(gardenPlotList.Last() - gardenPlotList.SkipLast(1).First());
+                //if (diffList.Count() > 11)
+                //    diffDiffList.Add(diffList.Last() - diffList.SkipLast(11).First());
                 //if (diffDiffList.Count() > 1)
                 //    diffDiffDiffList.Add(diffDiffList.Last() - diffDiffList.TakeLast(2).First());
                 newPositions = new HashSet<Day21Point>();
                 //Day21Print2(rocks, currentPositions);
 
                 //int length = Day21FindPattern(diffList);
-                if ((i + 1) % fullData.Item1 == 0)
+                //if ((i - 5) % (fullData.Item1 - 1) == 0)
+                //{
+                gardenPlotList.Add(currentPositions.Count());
+                Day21Print2(rocks, currentPositions);
+                Debug.WriteLine("i: " + i + " plots: " + currentPositions.Count());
+                Debug.WriteLine("plotlist: " + string.Join(',', gardenPlotList));
+                Debug.WriteLine("Start garden plots: " + currentPositions.Where(w => w.X >= 0 && w.X < maxX && w.Y >= 0 && w.Y < maxY).Count());
+
+                int fullLen = i - 6;
+                //int fullLen2 = i;
+                if (i >= 27)
                 {
-                    Day21Print2(rocks, currentPositions);
-                    Debug.WriteLine("i: " + i + " plots: " + currentPositions.Count());
-                    Debug.WriteLine("Calculated full plots: " +  (((i + 1) / fullData.Item1) * fullData.Item2 + (((i + 1) / fullData.Item1) - 1) * 4 * fullData.Item2));
+                    int fullPlots = 1;
+                    int plots = 0;
+                    for (int j = 0; j < (fullLen / 11); j++)
+                        fullPlots += 4 * j;
+                    if (((fullLen / 11) % 2 == 0 && fullLen % 2 == 0) || (fullLen / 11) % 2 == 1 && fullLen % 2 == 1)
+                        plots = fullData.Item3 * (fullLen / 11) * (fullLen / 11) + (fullPlots - (fullLen / 11) * (fullLen / 11)) * fullData.Item2;
+                    else
+                        plots = fullData.Item2 * (fullLen / 11) * (fullLen / 11) + (fullPlots - (fullLen / 11) * (fullLen / 11)) * fullData.Item3;
+
+
+
+                    //int top = currentPositions.Where(w => w.X >= 0 && w.X < maxX && w.Y < -(((fullLen / 11) - 1) * 11)).Count();
+                    //int topRight1 = ((fullLen2 / 11) - 1) * currentPositions.Where(w => w.X >= 11 && w.X < 11 + maxX && w.Y < -(((fullLen2 / 11) - 2) * 11) && w.Y >= -(((fullLen2 / 11) - 2) * 11 + maxY)).Count();
+                    //int topRight2 = (fullLen2 / 11) * currentPositions.Where(w => w.X >= 2 * 11 && w.X < 2 * 11 + maxX && w.Y < -(((fullLen2 / 11) - 2) * 11) && w.Y >= -(((fullLen2 / 11) - 2) * 11 + maxY)).Count();
+                    //int topLeft1 = ((fullLen2 / 11) - 1) * currentPositions.Where(w => w.X < 0 && w.X >= -maxX && w.Y < -(((fullLen2 / 11) - 2) * 11) && w.Y >= -(((fullLen2 / 11) - 2) * 11 + maxY)).Count();
+                    //int topLeft2 = (fullLen2 / 11) * currentPositions.Where(w => w.X < -11 && w.X >= -(maxX + 11) && w.Y < -(((fullLen2 / 11) - 2) * 11) && w.Y >= -(((fullLen2 / 11) - 2) * 11 + maxY)).Count();
+                    //int bottom = currentPositions.Where(w => w.X >= 0 && w.X < maxX && w.Y >= ((fullLen / 11) * 11)).Count();
+                    //int bottomRight1 = ((fullLen2 / 11) - 1) * currentPositions.Where(w => w.X >= 11 && w.X < 11 + maxX && w.Y >= (((fullLen2 / 11) - 1) * 11) && w.Y < (((fullLen2 / 11) - 1) * 11 + maxY)).Count();
+                    //int bottomRight2 = (fullLen2 / 11) * currentPositions.Where(w => w.X >= 11 * 2 && w.X < 11 * 2 + maxX && w.Y >= (((fullLen2 / 11) - 1) * 11) && w.Y < (((fullLen2 / 11) - 1) * 11 + maxY)).Count();
+                    //int bottomLeft1 = ((fullLen2 / 11) - 1) * currentPositions.Where(w => w.X < 0 && w.X >= -maxX && w.Y >= (((fullLen2 / 11) - 1) * 11) && w.Y < (((fullLen2 / 11) - 1) * 11 + maxY)).Count();
+                    //int bottomLeft2 = (fullLen2 / 11) * currentPositions.Where(w => w.X < -11 && w.X >= -(maxX + 11) && w.Y >= (((fullLen2 / 11) - 1) * 11) && w.Y < (((fullLen2 / 11) - 1) * 11 + maxY)).Count();
+                    //int left = currentPositions.Where(w => w.X < -(((fullLen / 11) - 1) * 11) && w.Y >= 0 && w.Y < maxY).Count();
+                    //int right = currentPositions.Where(w => w.X >= (fullLen / 11) * 11 && w.Y >= 0 && w.Y < maxY).Count();
+
+                    edgePlots.Add(currentPositions.Count() - plots);
+                    Debug.WriteLine("calc full gardens: " + fullPlots);
+                    Debug.WriteLine("calc full gardens plots: " + plots);
+                    //Debug.WriteLine("top: " + top + " topRight1: " + topRight1 + " topRight2: " + topRight2 + " topLeft1: " + topLeft1 + " topLeft2: " + topLeft2
+                    //    + " bottom: " + bottom + " bottomRight1: " + bottomRight1 + " bottomRight2: " + bottomRight2 + " bottomLeft1: " + bottomLeft1 + " bottomLeft2: " + bottomLeft2
+                    //    + " left: " + left + " right: " + right);
+                    //Debug.WriteLine("Total calculated plots: " + (plots + top + topRight1 + topRight2 + topLeft1 + topLeft2 + bottom + bottomRight1 + bottomRight2
+                    //    + bottomLeft1 + bottomLeft2 + left + right));
+                    //Debug.WriteLine("Calculated edge plots: " + (top + topRight1 + topRight2 + topLeft1 + topLeft2 + bottom + bottomRight1 + bottomRight2
+                    //    + bottomLeft1 + bottomLeft2 + left + right));
+                    Debug.WriteLine("Edge plots: " + (currentPositions.Count() - plots));
+                    Debug.WriteLine("Edge plotList: " + string.Join(',', edgePlots));
+
+
+                    //}
                 }
             }
             Debug.WriteLine(currentPositions.Count());
@@ -248,6 +297,30 @@ namespace AdventCalendar2023
         }
 
         private void Day21Print2(HashSet<Day21Point> rocks, HashSet<Day21Point> currentPositions)
+        {
+            int maxY = rocks.Max(m => m.Y) + 2;
+            int maxX = rocks.Max(m => m.X) + 2;
+            for (int y = currentPositions.Min(m => m.Y); y <= currentPositions.Max(m => m.Y); y++)
+            {
+                string line = string.Empty;
+                for (int x = currentPositions.Min(m => m.X); x <= currentPositions.Max(m => m.X); x++)
+                {
+                    if (currentPositions?.Contains<Day21Point>(new Day21Point { Y = y, X = x }) ?? false)
+                        line += "O";
+                    else if (rocks.Contains<Day21Point>(new Day21Point { Y = y < 0 ? maxY + y % maxY : y % maxY, X = x < 0 ? maxY + x % maxY : x % maxY }))
+                        line += "#";
+                    else
+                        line += ".";
+                    if ((x + 1) % maxX == 0)
+                        line += "|";
+                }
+                Debug.WriteLine(line);
+                if ((y + 1) % maxY == 0)
+                    Debug.WriteLine("".PadRight(line.Length, '-'));
+            }
+        }
+
+        private void Day21Print2(HashSet<Day21Point> rocks, List<Day21Point> currentPositions)
         {
             int maxY = rocks.Max(m => m.Y) + 2;
             int maxX = rocks.Max(m => m.X) + 2;
