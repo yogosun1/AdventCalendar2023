9c6d15ae0486ca5f4df5cd2725ea2a8847d3b675
Johan Björk <johanbjork@live.se>
2023-12-24 09:34:16 +0100

    .

--- AdventCalendar2023/WorkInProgress.cs
+++ AdventCalendar2023/WorkInProgress.cs
@@ -22,7 +22,73 @@ namespace AdventCalendar2023
         [TestMethod]
         public void Day24()
         {
-            List<string> inputList = File.ReadAllLines(@"Input\Day24.txt").ToList();
+            List<string> inputList = File.ReadAllLines(@"Input\Day24Test.txt").ToList();
+            List<Day24HailStone> hailstones = new List<Day24HailStone>();
+            foreach (string input in inputList)
+            {
+                List<string> inputSplit = input.Split('@').ToList();
+                List<string> leftSideSplit = inputSplit[0].Split(',').ToList();
+                List<string> rightSideSplit = inputSplit[1].Split(',').ToList();
+                Day24HailStone hailstone = new Day24HailStone
+                {
+                    X = decimal.Parse(leftSideSplit[0].Trim()),
+                    Y = decimal.Parse(leftSideSplit[1].Trim()),
+                    Z = decimal.Parse(leftSideSplit[2].Trim()),
+                    Vx = decimal.Parse(rightSideSplit[0].Trim()),
+                    Vy = decimal.Parse(rightSideSplit[1].Trim()),
+                    Vz = decimal.Parse(rightSideSplit[2].Trim()),
+                };
+                hailstone.Slope = hailstone.Vy / hailstone.Vx;
+                hailstone.YIntercept = hailstone.Slope * (hailstone.X + hailstone.Y);
+                hailstones.Add(hailstone);
+            }
+
+            int intersectCount = 0;
+            decimal xyMin = 200000000000000;
+            decimal xyMax = 400000000000000;
+            Day24HailStone line1, line2;
+            for (int h = 0; h < hailstones.Count(); h++)
+            {
+                line1 = hailstones[h];
+                for (int i = h + 1; i < hailstones.Count(); i++)
+                {
+                    line2 = hailstones[i];
+                    decimal x = (line2.YIntercept - line1.YIntercept) / (line1.Slope - line2.Slope);
+
+
+                    decimal A1 = line1.Vy;
+                    decimal B1 = line1.Vx;
+                    decimal C1 = A1 * line1.X + B1 * line1.Y;
+
+                    decimal A2 = line2.Vy;
+                    decimal B2 = line2.Vx;
+                    decimal C2 = A2 * line2.X + B2 * line2.Y;
+
+                    decimal delta = A1 * B2 - A2 * B1;
+
+                    if (delta == 0)
+                        throw new ArgumentException("Lines are parallel");
+
+                    decimal xI = (B2 * C1 - B1 * C2) / delta;
+                    decimal yI = (A1 * C2 - A2 * C1) / delta;
+                }
+            }
+            foreach (Day24HailStone hailstone in hailstones)
+            {
+
+            }
+        }
+
+        private class Day24HailStone
+        {
+            public decimal X { get; set; }
+            public decimal Y { get; set; }
+            public decimal Z { get; set; }
+            public decimal Vx { get; set; }
+            public decimal Vy { get; set; }
+            public decimal Vz { get; set; }
+            public decimal Slope { get; set; }
+            public decimal YIntercept { get; set; }
         }
 
         [TestMethod]
